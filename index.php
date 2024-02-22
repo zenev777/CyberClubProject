0f2bcb49e4f74b672d42a2fe7c11a017a0f47f88
Zhan <zhanenev6@gmail.com>
2024-02-22 22:55:19 +0200

    add h1

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>site</title>
+</head>
+<body>
+
+<header>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>FAQ</li>
+            <li>Contacts</li>
+        </ul>
+    </nav>
+</header>
+<article>
+    <section>
+        <h1>Welcome to CyberClub</h1>
+    </section>
+</article>
+
+</body>
+</html>
