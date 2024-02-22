df9a222913b4f37e4d20bc2c3b38329a21b1cb35
Zhan <zhanenev6@gmail.com>
2024-02-22 22:19:32 +0200

    index

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>site</title>
+</head>
+<body>
+
+<header>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>FAQ</li>
+            <li>Contacts</li>
+        </ul>
+    </nav>
+</header>
+<article>
+    <section>
+        <?php print "Hello World" ?>
+    </section>
+</article>
+
+</body>
+</html>
