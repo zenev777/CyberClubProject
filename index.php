91a0a3889a307e6f06e7097059cc41a487b09820
Zhan <zhanenev6@gmail.com>
2024-03-09 19:19:30 +0200

    video added

--- index.php
+++ index.php
@@ -13,30 +13,38 @@
         <nav>
             <ul>
                 <div class="logo-img">
-                <a href="">
-                    <li><img src="images/TRU.png" alt=""></li>
-                </a>
+                    <a href="">
+                        <li><img src="images/TRU.png" alt=""></li>
+                    </a>
                 </div>
-                
+
                 <div class="nav-btns">
-                <a href="">
-                    <li>Home</li>
-                </a>
-                <a href="FAQ.php">
-                    <li>FAQ</li>
-                </a>
-                <a href="contact.php">
-                    <li>Contacts</li>
-                </a>
+                    <a href="">
+                        <li>Home</li>
+                    </a>
+                    <a href="FAQ.php">
+                        <li>FAQ</li>
+                    </a>
+                    <a href="contact.php">
+                        <li>Contacts</li>
+                    </a>
                 </div>
-                
+
             </ul>
         </nav>
     </header>
+    <div class="vid">
+        <video autoplay muted loop id="myVideo">
+            <source src="images/vid.mp4" type="video/mp4">
+        </video>
+    </div>
 
     <section class="home-page">
+
         <article class="welcome-msg">
+
             <h1>Welcome to CyberClub</h1>
+
         </article>
         <article class="about-art">
             <h3>About</h3>
