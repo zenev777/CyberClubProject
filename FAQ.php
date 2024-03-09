1e944537b12cd4b18b39c1e4735fa1ff822be775
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-03-09 17:29:17 +0200

    Update FAQ.php

--- FAQ.php
+++ FAQ.php
@@ -38,37 +38,6 @@
         <article class="welcome-msg">
             <h1>Welcome to CyberClub</h1>
         </article>
-        <article class="about-art">
-            <h3>About</h3>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda voluptate ipsam molestias! Soluta in ipsum laboriosam quia laborum tempora, a nihil ab harum, dolor cum sint sapiente voluptate esse delectus.</p>
-        </article>
-        <article class="activities-art">
-            <div class="act">
-                <div class="act-img"><img src="images/artic.jpg" alt=""></div>
-                <div class="act-text">
-                    <h3>Activities</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, voluptatibus? Voluptatem ipsa, quae commodi aut natus earum odio. Voluptatum eveniet aliquid nam accusamus corrupti? Eum voluptate explicabo in facilis porro.</p>
-                    <a href="#">Join Event</a>
-                </div>
-            </div>
-            <div class="act">
-                <div class="act-text">
-                    <h3>Activities</h3>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis, voluptatibus? Voluptatem ipsa, quae commodi aut natus earum odio. Voluptatum eveniet aliquid nam accusamus corrupti? Eum voluptate explicabo in facilis porro.</p>
-                    <a href="#">Join Event</a>
-                </div>
-                <div class="act-img"><img src="images/artic.jpg" alt=""></div>
-            </div>
-            <div class="join-btn">
-                <a href="#">View all events</a>
-            </div>
-        </article>
-        <article class="join-club-btn">
-            <div>
-                <h4>Join out club</h4>
-                <a href="#">JOIN</a>
-            </div>
-        </article>
     </section>
 </body>
 
