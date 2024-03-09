c10937dec4bd12123af2f6c929ba89c079b720c6
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-03-09 17:25:50 +0200

    Contact page created

---
+++ contact.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>site</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <div class="logo-img">
+                <a href="">
+                    <li><img src="images/logo.png" alt=""></li>
+                </a>
+                </div>
+                
+                <div class="nav-btns">
+                <a href="index.php">
+                    <li>Home</li>
+                </a>
+                <a href="">
+                    <li>FAQ</li>
+                </a>
+                <a href="">
+                    <li>Contacts</li>
+                </a>
+                </div>
+                
+            </ul>
+        </nav>
+    </header>
+
+    <section class="home-page">
+        <article class="welcome-msg">
+            <h1>Welcome to CyberClub</h1>
+        </article>
+    </section>
+</body>
+
+</html>
