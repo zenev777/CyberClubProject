4967e3c52a714280ac56ebd2ee6ea3dde97cf5fd
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-03-19 18:30:28 +0200

    contact form

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/style.css">
-    <title>site</title>
+    <title>Контактна форма</title>
 </head>
 
 <body>
@@ -36,7 +36,20 @@
 
     <section class="home-page">
         <article class="welcome-msg">
-            <h1>Welcome to CyberClub</h1>
+            <h1>Свържете се с нас</h1>
+        </article>
+        <article class="contact-form">
+            <form>
+            <label for="fullName">Име и фамилия:</label><br>
+            <input type="text" id="fullName" name="fullName" placeholder="Вашите две имена" class="contact-inputs"><br>
+            <label for="email">Email</label><br>
+            <input type="text" id="email" name="email" class="contact-inputs" placeholder="Вашия Email адрес"><br>
+            <label for="subject">Относно:</label><br>
+            <input type="text" id="subject" name="subject" class="contact-inputs" placeholder="Темата на съобщението"><br>
+            <label for="message">Напишете съобщението си и ние в най-кратък период ще Ви отговорим:</label><br>
+            <textarea name="message" class="contact-inputs" placeholder="Напишете съобщението си тук"></textarea><br>
+            <button type="button" onclick="alert('Hello World!')">Изпрати <img src="images/icons8-arrow-24.png"></button> 
+            </form>
         </article>
     </section>
 </body>
